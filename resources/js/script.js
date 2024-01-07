2b1b54608bb026ef8e8a1f8e8ddfa820aba00b8a
louis.heraut <louis.heraut@inrae.fr>
2024-01-08 00:36:14 +0100

    efihjiezjfez

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,17 +1,21 @@
 document.addEventListener('DOMContentLoaded', function () {
-    fetch('../../components/header.html')
+   fetch('../../components/header.html')
         .then(response => response.text())
         .then(html => {
             document.getElementById('header').innerHTML = html;
-            var selectedTabId = localStorage.getItem('selectedTabId');
-            if (selectedTabId) {
-                var selectedTab = document.getElementById(selectedTabId);
-                if (selectedTab) {
-                    selectedTab.classList.add('selected');
-                }
+
+            var selectedTabIdsJson = localStorage.getItem('selectedTabId');
+            if (selectedTabIdsJson) {
+                var selectedTabIds = JSON.parse(selectedTabIdsJson);
+
+                selectedTabIds.forEach(function (tabId) {
+                    var selectedTab = document.getElementById(tabId);
+                    if (selectedTab) {
+                        selectedTab.classList.add('selected');
+                    }
+                });
             }
         });
-
     fetch('../../components/footer.html')
         .then(response => response.text())
         .then(html => {
@@ -34,39 +38,24 @@ function hideMenu(menuId) {
     }
 }
 
-// function selectTab(selectedTabId) {
-//     selectedTab = document.getElementById(selectedTabId);
-//     var tabs = selectedTab.parentNode.querySelectorAll('a');
-//     tabs.forEach(function (tab) {
-//         tab.classList.remove('selected');
-//     });
-//     selectedTab.classList.add('selected');
-
-//     console.log(selectedTab);
-    
-//     localStorage.setItem('selectedTabId', selectedTab.id);
-// }
-
 
 function selectTab(selectedTabIds) {
+    if (!Array.isArray(selectedTabIds)) {
+        selectedTabIds = [selectedTabIds];
+    }
+    // localStorage.setItem('selectedTabId', tab.id);
+    localStorage.setItem('selectedTabId', JSON.stringify(selectedTabIds));
+    
     selectedTabIds.forEach(function (tabId) {
         var tab = document.getElementById(tabId);
         if (tab) {
-            var tabs = tab.parentNode.querySelectorAll('a');
+	    var tabs = document.querySelectorAll('a.tab, a.subtab');
             tabs.forEach(function (t) {
-
-		console.log(selectedTabIds);
-		console.log(t.id);
-		console.log("");
 		if (!selectedTabIds.includes(t.id)) {
                     t.classList.remove('selected');
 		}
 	    });
             tab.classList.add('selected');
-            
-            console.log(tab);
-            
-            localStorage.setItem('selectedTabId', tab.id);
         }
     });
 }
